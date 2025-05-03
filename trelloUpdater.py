245ba6c07134fe7aca69de345411ccc2b78327e5
bwscp173 <benedictwardscp173@gmail.com>
2025-05-03 22:46:49 +0100

    console logging + error handling
    better console logging
    better error handling for requests

--- trelloUpdater.py
+++ trelloUpdater.py
@@ -2,7 +2,6 @@ import time
 import datetime
 import json
 import requests
-from pyasn1.debug import Printer
 
 with open("../trelloAPI.json","r") as f:
   trello_data = json.load(f)
@@ -11,7 +10,7 @@ with open("../githubAPI.json","r") as f:
 
 TRELLO_API = trello_data["TRELLO_APIKEY"]
 TRELLO_TOKEN = trello_data["TRELLO_TOKEN"]
-TRELLO_IDBOARD = trello_data["TRELLO_IDBOARD"]
+TRELLO_ID_BOARD = trello_data["TRELLO_ID_BOARD"]
 BOARD_NAME = 'github-testing'
 TRELLO_LIST = f"projectEuler push history"
 
@@ -20,14 +19,19 @@ GITHUB_PASSWORD = github_data["GITHUB_PASSWORD"]
 GITHUB_TOKEN = github_data["GITHUB_TOKEN"]
 GITHUB_PROJECTNAME = "projectEuler"
 
-def DisplayAll_idBoard():
+class RequestInvalid(Exception):
+  def __init__(self, errorMessage):
+    super().__init__(errorMessage)
+
+def get_time() -> str:
+  """returns yy:mm:dd hr:min:sec
+  will be used for logging."""
+  return str(datetime.datetime.now())[:-7]
+
+def display_all_id_board():
   url = "https://api.trello.com/1/members/me/boards"
 
-  query = {
-      'key': TRELLO_API,
-      'token': TRELLO_TOKEN,
-      'fields': 'name,id'
-  }
+  query = {'key': TRELLO_API,'token': TRELLO_TOKEN,'fields': 'name,id'}
 
   response = requests.get(url, params=query)
 
@@ -36,29 +40,20 @@ def DisplayAll_idBoard():
   for board in boards:
       print(f"{board['name']} -> {board['id']}")
 
-def CreateNewList(listName: str = "push history"):
+def create_new_list(listName: str = "push history"):
   """have a guess what this function does"""
   url = "https://api.trello.com/1/lists"
 
-  query = {
-    'name': listName,
-    'idBoard': TRELLO_IDBOARD,
-    'key': TRELLO_API,
-    'token': TRELLO_TOKEN
-  }
+  query = {'name': listName,'idBoard': TRELLO_ID_BOARD,'key': TRELLO_API,'token': TRELLO_TOKEN}
 
-  response = requests.request(
-    "POST",
-    url,
-    params=query
-  )
-  match response.status_code:
-    case 200:
-      print("sucessfully added list '{listName}'")
-  print(response)
+  response = requests.request("POST",url,params=query)
+  if response.status_code == 200:
+    print(f"[{get_time()}]successfully added list '{listName}'")
+  else:
+    raise RequestInvalid(f"Invalid request sent, status code: {response.status_code}")
 
-def GetAllLists():
-  url = f"https://api.trello.com/1/boards/{TRELLO_IDBOARD}/lists"
+def get_all_lists():
+  url = f"https://api.trello.com/1/boards/{TRELLO_ID_BOARD}/lists"
 
   query = {
       'key': TRELLO_API,
@@ -74,22 +69,31 @@ def GetAllLists():
   if response.status_code == 200:
     return response.json()
   else:
-    print("Invalid request sent, status code " + response.status_code)
-    return [{"name":"Invalid request IGNORE"}]
+    raise RequestInvalid(f"Invalid request sent, status code: {response.status_code}")
 
-def CheckListExists(listName: str = "push history") -> bool | str:
+def check_list_exists(listName: str = "push history") -> bool | str:
   """given a lists name will iterate through all list names and return if its found"""
-  response = GetAllLists()
-  for list in response:
-    if list["name"] == listName:
-      return list["id"]
+  try:
+    responses = get_all_lists()
+    print(f"[{get_time()}]request to get all lists succeeded")
+  except RequestInvalid as ex:
+    print(f"[{get_time()}]request to get all lists failed")
+    print(ex)
+    return False
+
+  for res in responses:
+    if 'name' in list(res.keys()) and 'id' in list(res.keys()):
+      if res["name"] == listName:
+        return res["id"]
+    else:
+      raise RequestInvalid("there is a malformed packet being sent from the server, it does not have the 'name' or 'id' key.")
   return False
 
-def CreateNewCard(idList: str, user,description: str, push_date: str):
+def create_new_card(idList: str, user,description: str, push_date: str):
   """
   the title of the card will always be the time of the function call with the user appended
 
-  description will apear when you click
+  description will show when you click
 
   example idList is '6810f1275ace42ee8ca3b6db' or '6810ef7b6c46693d6ed9c90c'."""
   url = "https://api.trello.com/1/cards"
@@ -98,27 +102,14 @@ def CreateNewCard(idList: str, user,description: str, push_date: str):
     "Accept": "application/json"
   }
 
-  query = {
-    'idList': idList,
-    'key': TRELLO_API,
-    'token': TRELLO_TOKEN,
-    'name': push_date + " - " + user,
-    'desc': description
-  }
+  query = {'idList': idList,'key': TRELLO_API,'token': TRELLO_TOKEN,'name': push_date + " - " + user,'desc': description}
 
-  response = requests.request(
-    "POST",
-    url,
-    headers=headers,
-    params=query
-  )
+  response = requests.request("POST",url,headers=headers,params=query)
 
   if response.status_code != 200:
-    print("CreateNewCards response code isnt 200: ", response.text)
+    raise RequestInvalid(f"create_new_cards response code is not 200: {response.status_code}")
 
-  #print(json.dumps(json.loads(response.text), sort_keys=True, indent=4, separators=(",", ": ")))
-
-def GetGitHubHistory(repoName: str = "") -> bool | list[dict]:
+def get_github_history(repoName: str = "") -> bool | list[dict]:
   """returns false if the status code is not 200
   else returns a list[dictionary] with all the history data for the repo"""
 
@@ -130,78 +121,80 @@ def GetGitHubHistory(repoName: str = "") -> bool | list[dict]:
   
   if response.status_code == 200:
     return response.json()
-    
-  print(response.status_code)
+  else:
+    raise RequestInvalid(f"invalid request info, status code: {response.status_code}")
 
-def CheckTrelloCard(listName: str, pushsummary, pushdate, pushauthor):
-  """returns true or false based on if the a trello card can be found with those values
-  pushsummary: the title"""
-  res = CheckListExists(listName)
+def check_trello_card(listName: str, push_summary, push_date, push_author):
+  """returns true or false based on if the trello card can be found with those values
+  push_summary: the title"""
+  res = check_list_exists(listName)
 
   if not res:
-    raise Exception("Cannot find that trello card idk what should happen just ignore ig")
+    raise RequestInvalid("Cannot find that trello card idk what should happen just ignore ig")
 
   if type(res) == str:
 
     url = f"https://api.trello.com/1/lists/{res}/cards"
 
-    headers = {
-      "Accept": "application/json"
-    }
+    headers = {"Accept": "application/json"}
 
-    query = {
-      'key': TRELLO_API,
-      'token': TRELLO_TOKEN
-    }
+    query = {'key': TRELLO_API,'token': TRELLO_TOKEN}
 
-    response = requests.request(
-      "GET",
-      url,
-      headers=headers,
-      params=query
-    )
+    response = requests.request("GET",url,headers=headers,params=query)
 
     if response.status_code != 200:
-      raise Exception ("error can not find a card with that listName or connection is down")
+      raise RequestInvalid (f"error can not find a card with that listName or connection is down, status code: {response.status_code}")
 
     json_res = json.loads(response.text)
     for response in json_res:
-      lines = response['desc'].split('\n')
+      if 'desc' in list(response.keys()):
+        lines = response['desc'].split('\n')
 
-      # for i in range(len(lines)):
-      #   print(f"line{i}: {lines[i]}")
-
-      if lines[1].find(pushauthor) != -1 and lines[2].find(pushdate) != -1 and lines[4].find(pushsummary) != -1:
-        #when its confirmed that a cards properties match up
-        return True
+        if lines[1].find(push_author) != -1 and lines[2].find(push_date) != -1 and lines[4].find(push_summary) != -1:
+          #when its confirmed that a cards properties match up
+          return True
+      else:
+        raise RequestInvalid("there is a malformed packet being sent from the server, it does not have the 'desc' key.")
   return False
 
-def AddTrelloGitHubHistoryCards():
-  res = GetGitHubHistory(GITHUB_PROJECTNAME)
+def add_trello_github_history_cards():
+  try:
+    res = get_github_history(GITHUB_PROJECTNAME)
+    print(f"[{get_time()}]request for getting github history succeeded")
+  except RequestInvalid as ex:
+    print(f"[{get_time()}]request to get all lists failed")
+    print(ex)
+    return False
+
   if type(res) == list:
     history = res
-
 
     for push in history:
       push_author = push['commit']['author']['name']
       push_date = push['commit']['author']['date'].replace("T"," ")[:-1]  # '2025-03-29T21:53:59Z' -> '2025-03-29 21:53:59'
       push_message = push['commit']["message"]
 
+      if value := not check_trello_card(TRELLO_LIST,push_message.split("\n")[0],push_date,push_author):
+        # print("\n"*2)
+        # print("="*20)
+        # print(f"author: {push_author}")
+        # print(f"push date: {push_date}")
+        # print(f"push message: {push_message}")
+        # print("="*20)
 
-      if value := not CheckTrelloCard(TRELLO_LIST,push_message.split("\n")[0],push_date,push_author):
-        print("\n"*2)
-        print("="*20)
-        print(f"author: {push_author}")
-        print(f"push date: {push_date}")
-        print(f"push message: {push_message}")
-        print("="*20)
+        try:
+          res = check_list_exists(f"{GITHUB_PROJECTNAME} push history")
+        except RequestInvalid as ex:
+          print(f"[{get_time()}]error {ex}")
+          continue
 
-
-        res = CheckListExists(f"{GITHUB_PROJECTNAME} push history")
         if type(res) == bool and res == False:
-          CreateNewList(f"{GITHUB_PROJECTNAME} push history")
-          res = CheckListExists(f"{GITHUB_PROJECTNAME} push history")
-
+          try:
+            create_new_list(f"{GITHUB_PROJECTNAME} push history")
+            res = check_list_exists(f"{GITHUB_PROJECTNAME} push history")
+          except RequestInvalid as ex:
+            print(f"[{get_time()}]error {ex}")
+            continue
         if type(res) == str:
           # then response is the id of the list
           idList = res
@@ -210,26 +203,36 @@ author: {push_author}
 push date: {push_date}
 push message:
 {push_message}"""
-          CreateNewCard(idList, push_author, push_message, push_date)
-      print("is there a card with that info already: ", not value)
-
-#print("did it find anything: ", CheckTrelloCard(TRELLO_LIST,"Initial commit","2025-03-24 21:29:14","bwscp173"))
-AddTrelloGitHubHistoryCards()
+          create_new_card(idList, push_author, push_message, push_date)
+      print(f"[{get_time()}]is there a card with that info already: {not value}")
 
 # testing adding new card:
-# res = CheckListExists("push history")
+# res = check_list_exists("push history")
 # if type(res) == bool and res == False:
-#   CreateNewList()
-#   res = CheckListExists("push history")
+#   create_new_list()
+#   res = check_list_exists("push history")
 
 # if type(res) == str:
 #   # then response is the id of the list
-#   CreateNewCard(res, "teammate1", "description be like, hey words")
+#   create_new_card(res, "teammate1", "description be like, hey words")
 
 
 # testing getting and adding labels:
-# CheckListExists()
+# check_list_exists()
 # print("\n"*3)
-# CreateNewList()
+# create_new_list()
 # print("\n"*3)
-# CheckListExists()
+# check_list_exists()
+
+if __name__ == "__main__":
+  print("▓" * 48)
+  print("author: benedict ward")
+  print("adds a gitHubs project history to a trello board")
+  print("▓"*80)
+  print("""▀█▀ █▀█ █▀▀ █░░ █░░ █▀█   ▄▀█ █░█ ▀█▀ █▀█   █▀▀ █▀█ █▀▄▀█ █▀▄▀█ █ ▀█▀
+░█░ █▀▄ ██▄ █▄▄ █▄▄ █▄█   █▀█ █▄█ ░█░ █▄█   █▄▄ █▄█ █░▀░█ █░▀░█ █ ░█░""")
+  print("▓" * 80)
+  t1 = time.time()
+  # add_trello_github_history_cards()
+  t2 = time.time()
+  print(f"time to execute: {t2-t1} seconds")
